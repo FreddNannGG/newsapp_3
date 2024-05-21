2f43d197f4f9202d86a2315f5dcad2e9b2854e26
FreddNannGG <alfergears@gmail.com>
2024-05-21 17:32:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/RequestManager.java
@@ -0,0 +1,32 @@
+package com.example.newsapp;
+
+import android.content.Context;
+
+import com.example.newsapp.Models.NewsApiResponse;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Query;
+
+public class RequestManager {
+    Context context;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public RequestManager(Context context) {
+        this.context = context;
+    }
+
+    public interface CallNewsApi {
+        Call<NewsApiResponse> callHeadlines(
+                @Query("country") String country,
+                @Query("category") String category,
+                @Query("q") String query,
+                @Query("apiKey") String api_key
+        );
+    }
+}
